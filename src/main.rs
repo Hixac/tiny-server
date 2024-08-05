d5868167eb59074bf84c0701d0b15d9e138faf85
Hixac <darkinbladechadshin92321@gmail.com>
2024-08-05 11:31:16 +0700

    Statically load and dynamically (baking into file)

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use tiny_http::{Header, Method, Request, Response, Server, StatusCode};
 
-fn form_response(request: Request, bytes: &[u8], content_type: &str, status_code: u16) -> std::io::Result<()> {
+fn form_response_dynamically(request: Request, bytes: &[u8], content_type: &str, status_code: u16) -> std::io::Result<()> {
 	let header = Header::from_bytes("Content-Type", content_type)
 		.expect("Shit fucked up header");
 	let response = Response::from_data(bytes).with_header(header).with_status_code(StatusCode(status_code));
@@ -8,24 +8,47 @@ fn form_response(request: Request, bytes: &[u8], content_type: &str, status_code
 	request.respond(response)
 }
 
+fn respond_404(request: Request) -> std::io::Result<()> {
+	form_response_dynamically(request, std::include_bytes!("404.html"), "text/html; charset=utf-8", 404)
+}
+
+fn respond_500(request: Request) -> std::io::Result<()> {
+	form_response_dynamically(request, std::include_bytes!("500.html"), "text/html; charset=utf-8", 500)
+}
+
+// I need statically load for perfoming the hotload
+fn form_response_statically(request: Request, path: &str, content_type: &str, status_code: u16) -> std::io::Result<()> {
+	let file = match std::fs::File::open(path) {
+		Ok(f) => f,
+		Err(err) => {
+			eprintln!("ERROR: file not found: {err}");
+			return respond_500(request);
+		}
+	};
+
+	let header = Header::from_bytes("Content-Type", content_type)
+		.expect("Shit fucked up header");
+	let response = Response::from_file(file).with_header(header).with_status_code(StatusCode(status_code));
+
+	request.respond(response)
+}
+
 fn explore(request: Request) -> std::io::Result<()> {
 	match (request.method(), request.url()) {
 		(Method::Get, "/main") => {
-			form_response(request, std::include_bytes!("index.html"), "text/html; charset=utf-8", 200)
+			form_response_statically(request, "index.html", "text/html; charset=utf-8", 200)
 		}
 		(Method::Get, "/index.css") => {
-			form_response(request, std::include_bytes!("index.css"), "text/css; charset=utf-8", 200)
+			form_response_statically(request, "index.css", "text/css; charset=utf-8", 200)
 		}
 		(Method::Get, "/index.js") => {
-			form_response(request, std::include_bytes!("index.js"), "text/javascript; charset=utf-8", 200)
+			form_response_statically(request, "index.js", "text/javascript; charset=utf-8", 200)
 		}
 		(Method::Get, "/404.css") => {
-			form_response(request, std::include_bytes!("404.css"), "text/css; charset=utf-8", 200)
+			form_response_dynamically(request, std::include_bytes!("404.css"), "text/css; charset=utf-8", 200)
 		}
 		_ => {
-			let html_bytes = std::include_bytes!("404.html");
-
-			form_response(request, html_bytes, "text/html; charset=utf-8", 404)
+			respond_404(request)
 		}
 	}
 }
